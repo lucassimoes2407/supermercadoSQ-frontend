a965ca80e456670f8b2071469135721b181dbd17
Ismael Zaccah - PopOS <ismael.zaccah@gmail.com>
2022-07-14 22:14:00 -0300

    fix: Create product works

--- src/pages/CreateProduct/index.jsx
+++ src/pages/CreateProduct/index.jsx
@@ -1,85 +1,130 @@
 import react from 'react';
-import { Box, Button, Input, TextField } from "@mui/material";
+import { Avatar, Box, Button, Container, createTheme, CssBaseline, Grid, TextField, Typography } from "@mui/material";
 import "./index.css";
 import UploadIcon from '@mui/icons-material/Upload';
-import CheckIcon from '@mui/icons-material/Check';
+import AddIcon from '@mui/icons-material/Add';
 import { postProduct } from '../../services/product'
-import backend_connection from '../../config/api';
+import { ThemeProvider } from 'styled-components';
+import CopyrightDevHub from '../../components/CopyrightDevHub';
+import { useSnack } from '../../hooks/useSnack';
+
+const theme = createTheme();
 
 const CreateProduct = () => {
+	const { snack, handleSnackOpen, handleSnackState } = useSnack();
+	const handleSubmit = async (event) => {
+		try {
+			event.preventDefault();
+			const data = new FormData(event.currentTarget);
 
-    const handleSubmit = async (event) => {
-        try {
-            event.preventDefault();
-            const data = new FormData(event.currentTarget);
+			let product = data.get('product');
+			let brand = data.get('brand');
+			let ingredients = data.get('ingredients');
+			let img = data.get('img');
+			let codUser = 5; // adicionar cod_usuario logado
+			
+			const response = await postProduct(product, brand, ingredients, img, codUser);
+			console.log(response);
+			
+			if(response.status == 200){
+				handleSnackState(
+					{...snack,
+						open: true,
+						message: response.data
+					}
+				)
+			}
+		} catch (e) {
+			console.log(e);
+			handleSnackState(
+				{...snack,
+					open: true,
+					message: "Ops, Algo deu errado!"
+				}
+			);
+		}
+	};
 
-            let product = data.get('product');
-            let brand = data.get('brand');
-            let ingredients = data.get('ingredients');
-            let codUser = 1;
+	return (
 
-            // console.log(process.env);
-            const { response } = await postProduct(product, brand, ingredients, codUser);
-            console.log(response);
-        } catch (e) {
-            console.log(e);
-        }
-    };
+		<ThemeProvider theme={theme}>
+			<Container component="main" maxWidth="xs">
+				<CssBaseline />
+				<Box
+					sx={{
+						marginTop: 8,
+						display: 'flex',
+						flexDirection: 'column',
+						alignItems: 'center',
+					}}
+				>
 
-    return (
-        <div className="createProduct__div">
-            <h1 >Novo Produto</h1>
-            <Box
-                component="form"
-                sx={{
-                    '& .MuiTextField-root': {
-                        m: 1,
-                        width: '95%',
-                        flexDirection: 'column'
-                    },
-                }}
-                noValidate
-                onSubmit={handleSubmit}
-                autoComplete="off"
-            >
-                <TextField id="product" label="Produto" variant="outlined" />
-                <TextField id="brand" label="Marca" variant="outlined" />
-                <TextField
-                    id="ingredients"
-                    label="Ingredientes"
-                    multiline
-                    minRows={3}
-                    maxRows={6}
-                // value={value}
-                // onChange={handleChange}
-                />
-                <Button
-                    variant="contained"
-                    component="label"
-                >
-                    <UploadIcon /> Enviar Foto
-                    <input
-                        accept="image/*"
-                        type="file"
-                        hidden
-                    />
+					<Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+						<AddIcon />
+					</Avatar>
+					<Typography component="h1" variant="h5">Novo Produto</Typography>
 
-                </Button>
+					<Box
+						component="form"
+						noValidate
+						onSubmit={(e) => handleSubmit(e)}
+						autoComplete="off"
+						sx={{ mt: 3 }}
+					>
+						<Grid container spacing={2}>
+							<Grid item xs={12}>
+								<TextField
+									name="product"
+									id="product"
+									label="Produto"
+									fullWidth
+									required
+									autoFocus
+								/>
+							</Grid>
 
-                <Button
-                    variant="contained"
-                    component="label"
-                >
-                    <CheckIcon /> Cadastrar
-                    <input
-                        type="submit"
-                        hidden
-                    />
-                </Button>
-            </Box>
-        </div>
+							<Grid item xs={12}>
+								<TextField
+									name="brand"
+									id="brand"
+									label="Marca"
+									fullWidth
+									required
+								/>
+							</Grid>
 
-    )
+							<Grid item xs={12}>
+								<TextField
+									name="ingredients"
+									id="ingredients"
+									label="Ingredientes"
+									fullWidth
+									multiline
+									minRows={3}
+									maxRows={6}
+								/>
+							</Grid>
+							<Grid item xs={12} container justifyContent="flex-start">
+								<Button component="label">
+									<UploadIcon /> Enviar Foto
+									<input name='img' id='img' hidden accept="image/*" multiple type="file" />
+								</Button>
+							</Grid>
+						</Grid>
+
+						<Button
+							type="submit"
+							fullWidth
+							variant="contained"
+							sx={{ mt: 3, mb: 2 }}
+						>
+							Cadastrar
+						</Button>
+					</Box>
+				</Box>
+				<CopyrightDevHub sx={{ mt: 5 }} />
+			</Container>
+		</ThemeProvider>
+	)
 }
-
 export default CreateProduct;
