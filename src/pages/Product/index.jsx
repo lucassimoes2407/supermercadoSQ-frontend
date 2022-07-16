fee167ae2b733b5ea799c82a5ca86fecba1abad4
Ismael Zaccah - PopOS <ismael.zaccah@gmail.com>
2022-07-16 17:52:03 -0300

    MEU DEUS

--- src/pages/Product/index.jsx
+++ src/pages/Product/index.jsx
@@ -4,50 +4,129 @@ import { getProductByCod } from "../../services/product";
 import ImageIcon from '@mui/icons-material/Image';
 import "./index.css"
 import { getUserByUserId } from "../../services/users";
+import { ThemeProvider } from "styled-components";
+import { Box, Container, createTheme, CssBaseline, Grid, Typography } from "@mui/material";
+import ProductCard from "../../components/ProductCard";
 
 const Product = () => {
-    const [product, setProduct] = useState({productInfo: {}, user: {}, restrictions: []});
+	const theme = createTheme();
 
-    const params = useParams();
+	const [product, setProduct] = useState({ productInfo: {}, user: {}, restrictions: [] });
 
-    useEffect(() => {
-        try {
-            (async () => {
-                const getProduct = await getProductByCod(params.cod);
-                const {product} = getProduct.data;
-                console.log(product);
-                console.log(product.productInfo.nome);
-                setProduct(product)
-            })();
-        }catch(e){
-            console.log(e);
-        }
-    }, [params]);
+	const params = useParams();
 
-    return(
-        <div className="product__div">
-            {(product.img_produto && 
-                <img className="product__img" src={product.img_produto} alt="" />)
-                || <div className="image__div--not-found">
-                    <ImageIcon sx={{ fontSize: 120 }} /> 
-                    Imagem não encontrada
-                    </div>
-            }
-            
-            <div className="product__div__content">
-                <p className="product__p" >{product.productInfo.marca || 'marca'}</p>
-                <h1 className="product__h1__nome">{(product.productInfo && product.productInfo.nome) || 'nome'}</h1>
+	useEffect(() => {
+		try {
+			(async () => {
+				const getProduct = await getProductByCod(params.cod);
+				const product = getProduct.data;
+				console.log(product);
+				console.log(product.productInfo.nome);
+				setProduct(product)
+			})();
+		} catch (e) {
+			console.log(e);
+		}
+	}, [params]);
 
-                <h3 className="product__h3">Contém:</h3>
+	return (
+		<ThemeProvider theme={theme}>
+			<Container component="main" maxWidth="xs">
+				<CssBaseline />
+				<Box
+					sx={{
+						display: 'flex',
+						flexDirection: 'column',
+						alignItems: 'flex-start',
+						textAlign: 'left'
+					}}
+				>
 
-                <h3 className="product__h3">Ingredientes:</h3>
-                <p>{product.productInfo.ingredientes || 'ingredientes'}</p>
+					<div className="product__div">
+						{(product.img_produto && <img className="product__img" src={product.img_produto} alt="" />)
+							|| <div className="image__div--not-found">
+								<ImageIcon sx={{ fontSize: 120 }} />
+								Imagem não encontrada
+							</div>
+						}
+					</div>
 
-                <h3 className="product__h3">Autor:</h3>
-                <p>{product.user.username || 'nome usuário'}</p>
-            </div>
-        </div>
-    )
+					<Typography
+						variant="h1"
+						fontSize={30}
+						color="primary"
+					>
+						<b>{(product.productInfo && product.productInfo.nome) || 'nome'}</b>
+					</Typography>
+
+
+					<Typography
+						variant="subtitle1"
+						color="primary"
+					>
+						<b>{(product.productInfo && product.productInfo.marca) || 'marca'}</b>
+					</Typography>
+
+
+					<Typography
+						mt={2}
+						variant="h2"
+						fontSize={18}
+						color="primary"
+					>
+						<b>Contém:</b>
+					</Typography>
+						{product.restrictions.map((restriction) => {
+							return (restriction.nome_restricao)
+						})}
+					<Typography
+						variant="body1"
+						color="primary"
+					>
+						Não existem Restrições associadas a este Produto
+					</Typography>
+
+
+
+
+
+					<Typography
+						mt={2}
+						variant="h2"
+						fontSize={18}
+						color="primary"
+					>
+						<b>Ingredientes:</b>
+					</Typography>
+
+					<Typography
+						variant="body1"
+						color="primary"
+					>
+						{product.productInfo.ingredientes || 'ingredientes'}
+					</Typography>
+
+
+
+					<Typography
+						mt={2}
+						variant="h2"
+						fontSize={18}
+						color="primary"
+					>
+						<b>Autor:</b>
+					</Typography>
+
+					<Typography
+						variant="body1"
+						color="primary"
+					>
+						<b>{product.user.username || 'nome usuário'}</b>
+					</Typography>
+				</Box>
+			</Container>
+		</ThemeProvider>
+	)
 }
 
 export default Product;
