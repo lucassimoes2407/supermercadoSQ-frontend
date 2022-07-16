2e8badb91628fdabd5080e2cec985e4773536324
Ismael Zaccah - PopOS <ismael.zaccah@gmail.com>
2022-07-16 14:06:52 -0300

    fix: theme colors apply

--- src/styles/materialTheme.js
+++ src/styles/materialTheme.js
@@ -1,21 +1,19 @@
 import { createTheme } from "@mui/material";
 
 export const theme = createTheme({
-    palette: {
-      type: 'light',
-      primary: {
-        main: '#12788c',
-        light: '#3C828E',
-      },
-      secondary: {
-        main: '#e60050',
-      },
-      error: {
-        main: '##ff3d00',
-      },
-      warning: {
-        main: '#ea9814',
-      },
-      divider: '#12788c',
+  palette: {
+    type: 'light',
+    primary: {
+      main: '#12788c',
     },
+    secondary: {
+      main: '#d80061',
+    },
+    error: {
+      main: '##ff3d00',
+    },
+    warning: {
+      main: '#ea9814',
+    },
+  },
 });
