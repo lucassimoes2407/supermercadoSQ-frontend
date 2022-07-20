417edd1f89064b37f5ac781fe2cb734267e9839c
Ismael Zaccah - PopOS <ismael.zaccah@gmail.com>
2022-07-20 20:42:27 -0300

    fix: adjusted footer

--- src/components/CopyrightDevHub/index.jsx
+++ src/components/CopyrightDevHub/index.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Typography, Link, Box } from "@mui/material";
+import { Typography, Link, Box, Divider } from "@mui/material";
 import './index.css'
 
 import imagemDevHub from "../../assets/logo-devhub-mono-white.png";
@@ -7,24 +7,29 @@ import imagemDevHub from "../../assets/logo-devhub-mono-white.png";
 const CopyrightDevHub = (props) => {
   return (
     <Box
-      backgroundColor="primary.light"
+      backgroundColor="primary.main"
       display="flex"
       justifyContent="center"
       alignItems="center"
-      position= "fixed"
+      position="fixed"
       gap={2}
       bottom={0}
-      >
+      width="100%"
+      height={50}
+    >
       <img width={80} src={imagemDevHub} alt="DevHub" />
+      
       <Typography
-        mt={2}
+        mt="auto"
+        mb="auto"
         variant="h1"
         fontSize={16}
         color="primary.contrastText"
+        fontWeight={500}
       >
-        Atlântico Academy Bootcamp -
+        {"Atlântico Academy Bootcamp - "}
         <Typography variant="a" href="https://github.com/lucassimoes2407/supermercadoSQ-frontend" target="_ablank">
-          Equipe DevHub
+          {"Equipe DevHub"}
         </Typography>{' © '}
         {new Date().getFullYear()}
         {'.'}
