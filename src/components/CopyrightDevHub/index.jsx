5c3eb1c552afeccede64c0548d824c4fd3b60742
Ismael Zaccah - PopOS <ismael.zaccah@gmail.com>
2022-07-20 19:43:29 -0300

    new footer

--- src/components/CopyrightDevHub/index.jsx
+++ src/components/CopyrightDevHub/index.jsx
@@ -1,22 +1,35 @@
 import React from "react";
-import { Typography, Link } from "@mui/material";
+import { Typography, Link, Box } from "@mui/material";
 import './index.css'
 
-import imagemDevHub from "../../assets/logo-devhub-darkmode.png";
+import imagemDevHub from "../../assets/logo-devhub-mono-white.png";
 
 const CopyrightDevHub = (props) => {
   return (
-    <footer>
-      <img src={imagemDevHub} alt="DevHub" />
-        <p>
-          {'Atlântico Academy Bootcamp - '}
-          <a color="inherit" href="https://github.com/lucassimoes2407/supermercadoSQ-frontend" target="_ablank">
-            Equipe DevHub 
-          </a>{' © '}
-          {new Date().getFullYear()}
-          {'.'}
-        </p>    
-    </footer>
+    <Box
+      backgroundColor="primary.light"
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      position= "fixed"
+      gap={2}
+      bottom={0}
+      >
+      <img width={80} src={imagemDevHub} alt="DevHub" />
+      <Typography
+        mt={2}
+        variant="h1"
+        fontSize={16}
+        color="primary.contrastText"
+      >
+        Atlântico Academy Bootcamp -
+        <Typography variant="a" href="https://github.com/lucassimoes2407/supermercadoSQ-frontend" target="_ablank">
+          Equipe DevHub
+        </Typography>{' © '}
+        {new Date().getFullYear()}
+        {'.'}
+      </Typography>
+    </Box>
   );
 }
 
